21d78fc035319c1005e7f3351a3fe3f560983d32
Emir Herrera <emir.herrera@gmail.com>
2023-07-19 23:47:52 -0600

    add: dev: Modularized CSS

--- app/index.js
+++ app/index.js
@@ -7,6 +7,10 @@ function isStandalone(props) {
   return props.type === 'standalone';
 }
 
+function isNullType(props){
+  return !(props.naked)
+}
+
 function isPolymer(props) {
   return props.framework === 'polymer';
 }
@@ -17,70 +21,87 @@ function generateClassName(name) {
   }, '');
 }
 
-const PROMPTS = [
-  {
-    name: 'type',
-    type: 'list',
-    choices: [
-      { name: 'Standalone', value: 'standalone' },
-      { name: 'Part of an app', value: 'naked' }
-    ],
-    required: true,
-    message: 'Is this a standalone element, or part of an app?'
-  },
-  {
-    name: 'name',
-    type: 'input',
-    required: true,
-    message: 'Give it a tag name (min two words separated by dashes)',
-    validate: str => /^([a-z])(?!.*[<>])(?=.*-).+$/.test(str)
-  },
-  {
-    name: 'description',
-    when: isStandalone,
-    type: 'input',
-    message: 'Tell me all about it...'
-  },
-  {
-    name: 'framework',
-    type: 'list',
-    choices: [
-      { name: 'Polymer 2', value: 'polymer' },
-      { name: 'No framework', value: 'none' }
-    ],
-    required: true,
-    message: 'Which WC framework do you want to include?'
-  },
-  {
-    name: 'includeTemplate',
-//    when: isPolymer,
-    type: 'confirm',
-    message: 'Will your element need a shadow DOM template?',
-    default: true
-  },
-  {
-    name: 'author',
-    when: isStandalone,
-    type: 'input',
-    required: true,
-    message: 'Who wrote it?'
-  },
-  {
-    name: 'github',
-    when: isStandalone,
-    type: 'input',
-    required: true,
-    message: 'What GitHub org will it live in?'
-  }
-];
-
 class GeneratorWebComponent extends Yeoman {
+  PROMPTS = [];
+  initializing() {
+    this.PROMPTS = [
+      {
+        name: 'type',
+        type: 'list',
+        choices: [
+          { name: 'Standalone', value: 'standalone' },
+          { name: 'Part of an app', value: 'naked' }
+        ],
+        when: !(this.options.naked),
+        //default:'naked',
+        required: true,
+        message: 'Is this a standalone element, or part of an app?'
+      },
+      {
+        name: 'name',
+        type: 'input',
+        required: true,
+        message: 'Give it a tag name (min two words separated by dashes)',
+        validate: str => /^([a-z])(?!.*[<>])(?=.*-).+$/.test(str),
+        when:!(this.options.name)
+        //default:this.options.name
+      },
+      {
+        name: 'description',
+        when: isStandalone,
+        type: 'input',
+        message: 'Tell me all about it...'
+      },
+      {
+        name: 'framework',
+        type: 'list',
+        choices: [
+          { name: 'Polymer 2', value: 'polymer' },
+          { name: 'No framework', value: 'none' }
+        ],
+        required: true,
+        message: 'Which WC framework do you want to include?',
+        when: !(this.options.framework)
+      },
+      {
+        name: 'includeTemplate',
+        //    when: isPolymer,
+        when: !(this.options.includeTemplate),
+        type: 'confirm',
+        message: 'Will your element need a shadow DOM template?',
+        default: true
+      },
+      {
+        name: 'author',
+        when: isStandalone,
+        type: 'input',
+        required: true,
+        message: 'Who wrote it?'
+      },
+      {
+        name: 'github',
+        when: isStandalone,
+        type: 'input',
+        required: true,
+        message: 'What GitHub org will it live in?'
+      }
+    ];
+  }
+
+  constructor (args, opts){
+    super(args,opts)
+    this.option("name", {desc: 'Web Component name', type: String, default: "web-component"  });
+    this.option("framework", {desc: 'Framework', type: String, default: "none"  });
+    this.option("includeTemplate", {desc: 'Include shadow DOM', type: Boolean  });
+    this.option("naked", {desc: 'Single component', type: Boolean, default: false });
+  }
+
 
   prompting() {
     const done = this.async();
 
-    return this.prompt(PROMPTS).then(answers => {
-      this.props = answers;
+    return this.prompt(this.PROMPTS).then(answers => {
+      this.props = {...answers, ...(this.options)};
       done();
     });
   }
@@ -103,16 +124,22 @@ class GeneratorWebComponent extends Yeoman {
     const { name } = this.props;
 
     // Write & rename element src
-/*    this.fs.copyTpl(*/
-      /*this.templatePath('element.html'),*/
-      /*this.destinationPath(`${name}.html`),*/
-      /*this*/
+    /*    this.fs.copyTpl(*/
+    /*this.templatePath('element.html'),*/
+    /*this.destinationPath(`${name}.html`),*/
+    /*this*/
     /*);*/
     this.fs.copyTpl(
       this.templatePath('element.js'),
       this.destinationPath(`${name}.js`),
       this
     );
+    this.fs.copyTpl(
+      this.templatePath('element.css'),
+      this.destinationPath(`${name}.css`),
+      this
+    );
+
 
     // Write additional files only for standalone elements
     if (!isStandalone(this.props)) {
--- app/templates/element.js
+++ app/templates/element.js
@@ -1,19 +1,26 @@
 <% if (props.type === 'standalone' && props.framework === 'polymer') { %><link rel="import" href="../polymer/polymer-element.html">
 <% } %><% if (props.includeTemplate) { %>
 <%= props.class %>Template = document.createElement("template");
+stylesheet = document.createElement("link");
 
+stylesheet.setAttribute("ref", "stylesheet");  
+stylesheet.setAttribute("href", "<%=props.name%>.css")
 <%= props.class %>Template.innerHTML = `
-<dom-module id="<%= props.name %>">
-  <template>
-    <style>
-
-    </style>
-
-  </template>
-</dom-module>
+<template id="<%= props.name %>">
+</template>
 `
 <% } %>
 class <%= props.class %> extends <% if (props.framework === 'polymer' ) { %>Polymer.Element<% } else { %>HTMLElement<% } %> {
+  constructor() {
+    super();
+  }
+
+  connectedCallback(){
+    const shadowRoot = this.attachShadow({ mode: 'closed' });
+
+    shadowRoot.appendChild(<%=props.class%>Template.content);
+    shadowRoot.appendChild(stylesheet);
+  }
   static get is() {
     return '<%= props.name %>';
   }
