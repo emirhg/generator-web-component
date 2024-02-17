7bb42fdf3bd7717ef487206fb5e926a774839cc8
Emir Herrera <emir.herrera@gmail.com>
2024-02-16 22:10:40 -0600

    new: dev: Implement Pug template for Shadow DOM on custom element

--- app/templates/src/index.js
+++ app/templates/src/index.js
@@ -1,4 +1,5 @@
-class <%= props.class %> extends <% if (props.framework === 'polymer' ) { %>Polymer.Element<% } else { %>HTMLElement<% } %> {
+const template = require("./html-template.pug");
+class <%= props.class %> extends HTMLElement {
 
   constructor(){
     super();
@@ -6,53 +7,16 @@ class <%= props.class %> extends <% if (props.framework === 'polymer' ) { %>Poly
   // Specify observed attributes so that
   // attributeChangedCallback will work
   static get observedAttributes() {
-    /*
-     *return ["color", "size"];
-     */
   }
 
   static get is() {
     return '<%= props.name %>';
   }
 
-  connectedCallback(){
-    // Create some CSS to apply to the shadow dom
-/*
- *    const style = document.createElement("link");
- *    console.log(style.isConnected);
- *    style.setAttribute("rel", "stylesheet");
- *    style.setAttribute("href", "style.css");
- *
- *    // Create a shadow root
- *    const shadow = this.attachShadow({ mode: "open" });
- *
- *    // Create spans
- *    // Take attribute content and put it inside the info span
- *    const text = this.getAttribute("data-text");
- *
- *    // Insert icon
- *    let imgUrl;
- *    if (this.hasAttribute("img")) {
- *      imgUrl = this.getAttribute("img");
- *    } else {
- *      imgUrl = "img/default.png";
- *    }
- *
- *    const innerHTML = `<span class="wrapper"><span class="icon" tabindex="0"><img src="${imgUrl}"></span><span class="info">${text}</span></span>`
- *
- *    const template = document.createElement("template");
- *    template.innerHTML = innerHTML;
- *
- *    const wrapper = template.content.cloneNode(true);
- *
- *    const img = document.createElement("img");
- *    img.src = imgUrl;
- *
- *    // Attach the created elements to the shadow dom
- *    shadow.appendChild(style);
- *    console.log(style.isConnected);
- *    shadow.appendChild(wrapper);
- */
+  connectedCallback() {
+    // Create a shadow root
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.innerHTML = template;
   }
 
   disconnectedCallback() {
